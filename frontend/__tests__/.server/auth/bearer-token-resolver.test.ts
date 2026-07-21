400c1bab3da72f51aed89133c7229ecaf25ea1f1
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2026-07-21 12:23:12 -0300

    fix(frontend): improve bearer token resolution to handle extra spaces in authorization header

--- frontend/__tests__/.server/auth/bearer-token-resolver.test.ts
+++ frontend/__tests__/.server/auth/bearer-token-resolver.test.ts
@@ -13,6 +13,16 @@ describe('DefaultBearerTokenResolver', () => {
     expect(bearerTokenResolver.resolve(request)).toEqual(token);
   });
 
+  it('should resolve a bearer token from the authorization header with extra spaces', () => {
+    const bearerTokenResolver = new DefaultBearerTokenResolver();
+
+    const token = '00000000-0000-0000-0000-000000000000';
+    const headers = { authorization: `Bearer    ${token}` };
+    const request = new Request('https://example.com', { headers: headers });
+
+    expect(bearerTokenResolver.resolve(request)).toEqual(token);
+  });
+
   it('should return undefined if the authorization header is not set', () => {
     const bearerTokenResolver = new DefaultBearerTokenResolver();
     const request = new Request('https://example.com');
