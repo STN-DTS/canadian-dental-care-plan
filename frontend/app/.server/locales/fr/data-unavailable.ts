0e11d5639413ffdd00f4eb4975d0e3f17e9d82f2
Shawn <azuneko1110@gmail.com>
2026-08-13 09:38:58 -0400

    update data unavailable page

--- frontend/app/.server/locales/fr/data-unavailable.ts
+++ frontend/app/.server/locales/fr/data-unavailable.ts
@@ -1,10 +1,11 @@
 const ns = {
   pageTitle: "Vous n'êtes pas inscrit au Régime canadien de soins dentaires",
-  serviceEligible: "Si vous avez présenté une demande récemment, il est possible qu'un certain temps s'écoule avant que vos renseignements apparaissent dans Mon dossier Service Canada. Vous pouvez <statusCheckerLink>vérifier l'état</statusCheckerLink> de votre demande en cours de traitement.",
+  notEnrolled: "Il semble que vous n'êtes pas actuellement inscrit au Régime canadien de soins dentaires.",
+  recentlyApplied: "Si vous avez récemment présenté une demande, il pourrait avoir un court délai avant que vos informations apparaissent dans « Mon dossier Service Canada ».",
   doYouQualifyHref: "https://www.canada.ca/fr/services/prestations/dentaire/regime-soins-dentaires/admissibilite.html",
-  otherEnquiry: "Si vous n'avez pas présenté de demande, découvrez si vous êtes admissible au <cdcpLink>Régime canadien de soins dentaires</cdcpLink>.",
+  howToApply: "Si vous voulez présenter une demande, vous pouvez trouver si vous êtes admissible sur la page <cdcpLink>Régime canadien de soins dentaires</cdcpLink>.",
   contactUsHref: "https://www.canada.ca/fr/services/prestations/dentaire/regime-soins-dentaires/contactez.html",
-  serviceDelay: "<contactLink>Communiquez avec nous</contactLink> pour toute autre question ou demande de renseignements.",
+  otherEnquiry: "Pour toute autre question ou demande de renseignements, vous pouvez <contactLink>contacter</contactLink> le Régime canadien de soins dentaires.",
   backButton: "Retour au tableau de bord",
 } as const;
 
