1dfe81ce1b36422a0bfd1511d1c96b144ae2f5dc
Shawn <azuneko1110@gmail.com>
2026-08-25 11:11:45 -0400

    use fieldSet and legend

--- frontend/app/routes/public/application/spokes/marital-status.tsx
+++ frontend/app/routes/public/application/spokes/marital-status.tsx
@@ -215,10 +215,8 @@ export default function ApplicationSpokeMaritalStatus({ loaderData, params }: Ro
               />
 
               {(selectedMaritalStatus === MARITAL_STATUS_CODE_COMMON_LAW || selectedMaritalStatus === MARITAL_STATUS_CODE_MARRIED) && (
-                <section aria-labelledby="spouse-or-commonlaw-heading">
-                  <h2 id="spouse-or-commonlaw-heading" className="font-lato mb-6 text-2xl font-bold">
-                    {t(($) => $.maritalStatus.spouseOrCommonlaw)}
-                  </h2>
+                <fieldset className="mb-6 space-y-4">
+                  <legend className="font-lato mb-6 text-2xl font-bold">{t(($) => $.maritalStatus.spouseOrCommonlaw)}</legend>
                   <p className="mb-4">{t(($) => $.maritalStatus.provideSin)}</p>
                   <p className="mb-6">{t(($) => $.maritalStatus.requiredInformation)}</p>
                   <InputPatternField
@@ -255,7 +253,7 @@ export default function ApplicationSpokeMaritalStatus({ loaderData, params }: Ro
                   >
                     {t(($) => $.maritalStatus.confirmCheckbox)}
                   </InputCheckbox>
-                </section>
+                </fieldset>
               )}
             </div>
             <div className="flex flex-row-reverse flex-wrap items-center justify-end gap-3">
