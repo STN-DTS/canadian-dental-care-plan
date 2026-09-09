f8a0a0eac13cb3177b5e1bc4f0e5e31b58e6d0e7
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2026-09-09 13:04:42 -0300

    fix(frontend): update how-to-apply and other enquiry text

--- frontend/app/.server/locales/en/data-unavailable.ts
+++ frontend/app/.server/locales/en/data-unavailable.ts
@@ -3,9 +3,9 @@ const ns = {
   notEnrolled: "It looks like you're not currently enrolled in the Canadian Dental Care Plan.",
   recentlyApplied: "If you have recently applied, there could be a short delay with your information appearing in My Service Canada Account.",
   doYouQualifyHref: "https://www.canada.ca/en/services/benefits/dental/dental-care-plan/qualify.html",
-  howToApply: "If you would like to apply, find out if you're eligible through the <cdcpLink>Canadian Dental Care Plan</cdcpLink> page.",
+  howToApply: "If you would like to apply, find out if you're eligible through the <cdcpLink>Do you qualify</cdcpLink> page.",
   contactUsHref: "https://www.canada.ca/en/services/benefits/dental/dental-care-plan/contact.html",
-  otherEnquiry: "For other questions or enquiries, you can <contactLink>contact</contactLink> the Canadian Dental Care Plan.",
+  otherEnquiry: "For other questions or enquiries, you can <contactLink>contact the Canadian Dental Care Plan</contactLink>.",
   backButton: "Return to Dashboard",
 } as const;
 
--- frontend/app/.server/locales/fr/data-unavailable.ts
+++ frontend/app/.server/locales/fr/data-unavailable.ts
@@ -3,9 +3,9 @@ const ns = {
   notEnrolled: "Il semble que vous n'êtes pas actuellement inscrit au Régime canadien de soins dentaires.",
   recentlyApplied: "Si vous avez récemment présenté une demande, il pourrait avoir un court délai avant que vos informations apparaissent dans « Mon dossier Service Canada ».",
   doYouQualifyHref: "https://www.canada.ca/fr/services/prestations/dentaire/regime-soins-dentaires/admissibilite.html",
-  howToApply: "Si vous voulez présenter une demande, vous pouvez trouver si vous êtes admissible sur la page <cdcpLink>Régime canadien de soins dentaires</cdcpLink>.",
+  howToApply: "Si vous voulez présenter une demande, vous pouvez trouver si vous êtes admissible sur la page <cdcpLink>Êtes-vous admissible</cdcpLink>.",
   contactUsHref: "https://www.canada.ca/fr/services/prestations/dentaire/regime-soins-dentaires/contactez.html",
-  otherEnquiry: "Pour toute autre question ou demande de renseignements, vous pouvez <contactLink>contacter</contactLink> le Régime canadien de soins dentaires.",
+  otherEnquiry: "Pour toute autre question ou demande de renseignements, vous pouvez <contactLink>contacter le Régime canadien de soins dentaires</contactLink>.",
   backButton: "Retour au tableau de bord",
 } as const;
 
